95dc2d3b7bd277c1dcc4471983cc03470a376a9d
raymonddefade <raydefade@gmail.com>
2022-07-05 21:21:58 -0400

    RJD: updated files

--- src/main/java/com/mindex/challenge/data/Employee.java
+++ src/main/java/com/mindex/challenge/data/Employee.java
@@ -1,9 +1,5 @@
 package com.mindex.challenge.data;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-
 import java.util.List;
 
 
